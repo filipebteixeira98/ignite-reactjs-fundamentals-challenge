bc96a265520165713888b3d8147673067eb95576
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-05-10 18:12:11 -0300

    chore: finish challenge

--- src/__tests__/components/TaskList.spec.tsx
+++ src/__tests__/components/TaskList.spec.tsx
@@ -11,20 +11,20 @@ describe('App Page', () => {
 
     fireEvent.change(taskInput, {
       target: {
-        value: 'Desafio ReactJS Ignite'
-      }
+        value: 'Desafio ReactJS Ignite',
+      },
     });
     fireEvent.click(addTaskButton);
 
     const addedFirstTaskTitle = screen.getByText('Desafio ReactJS Ignite');
 
     expect(addedFirstTaskTitle).toHaveTextContent('Desafio ReactJS Ignite');
-    expect(addedFirstTaskTitle.parentElement).not.toHaveClass('completed')
+    expect(addedFirstTaskTitle.parentElement).not.toHaveClass('completed');
 
     fireEvent.change(taskInput, {
       target: {
-        value: 'Beber água'
-      }
+        value: 'Beber água',
+      },
     });
     fireEvent.click(addTaskButton);
 
@@ -32,11 +32,11 @@ describe('App Page', () => {
 
     expect(addedFirstTaskTitle).toBeInTheDocument();
     expect(addedFirstTaskTitle).toHaveTextContent('Desafio ReactJS Ignite');
-    expect(addedFirstTaskTitle.parentElement).not.toHaveClass('completed')
+    expect(addedFirstTaskTitle.parentElement).not.toHaveClass('completed');
 
     expect(addedSecondTaskTitle).toHaveTextContent('Beber água');
-    expect(addedSecondTaskTitle.parentElement).not.toHaveClass('completed')
-  })
+    expect(addedSecondTaskTitle.parentElement).not.toHaveClass('completed');
+  });
 
   it('should not be able to add a task with a empty title', () => {
     render(<TaskList />);
@@ -51,16 +51,16 @@ describe('App Page', () => {
 
     fireEvent.change(taskInput, {
       target: {
-        value: 'Desafio ReactJS Ignite'
-      }
+        value: 'Desafio ReactJS Ignite',
+      },
     });
-    
+
     fireEvent.click(addTaskButton);
 
     const addedFirstTaskTitle = screen.getByText('Desafio ReactJS Ignite');
 
     expect(addedFirstTaskTitle).toHaveTextContent('Desafio ReactJS Ignite');
-  })
+  });
 
   it('should be able to remove a task', async () => {
     render(<TaskList />);
@@ -70,31 +70,33 @@ describe('App Page', () => {
 
     fireEvent.change(taskInput, {
       target: {
-        value: 'Desafio ReactJS Ignite'
-      }
+        value: 'Desafio ReactJS Ignite',
+      },
     });
     fireEvent.click(addTaskButton);
 
     fireEvent.change(taskInput, {
       target: {
-        value: 'Beber água'
-      }
+        value: 'Beber água',
+      },
     });
     fireEvent.click(addTaskButton);
 
     const addedFirstTaskTitle = screen.getByText('Desafio ReactJS Ignite');
     const addedSecondTaskTitle = screen.getByText('Beber água');
 
-    expect(addedFirstTaskTitle).toBeInTheDocument()
+    expect(addedFirstTaskTitle).toBeInTheDocument();
     expect(addedSecondTaskTitle).toBeInTheDocument();
 
-    const [addedFirstTaskRemoveButton] = screen.getAllByTestId('remove-task-button');
+    const [addedFirstTaskRemoveButton] = screen.getAllByTestId(
+      'remove-task-button'
+    );
 
     fireEvent.click(addedFirstTaskRemoveButton);
 
     expect(addedFirstTaskTitle).not.toBeInTheDocument();
     expect(addedSecondTaskTitle).toBeInTheDocument();
-  })
+  });
 
   it('should be able to check a task', () => {
     render(<TaskList />);
@@ -104,22 +106,22 @@ describe('App Page', () => {
 
     fireEvent.change(taskInput, {
       target: {
-        value: 'Desafio ReactJS Ignite'
-      }
+        value: 'Desafio ReactJS Ignite',
+      },
     });
     fireEvent.click(addTaskButton);
 
     fireEvent.change(taskInput, {
       target: {
-        value: 'Beber água'
-      }
+        value: 'Beber água',
+      },
     });
     fireEvent.click(addTaskButton);
 
     const [addedFirstTask, addedSecondTask] = screen.getAllByTestId('task');
 
     if (addedFirstTask.firstChild) {
-      fireEvent.click(addedFirstTask.firstChild)
+      fireEvent.click(addedFirstTask.firstChild);
     }
 
     expect(addedFirstTask).toBeInTheDocument();
@@ -127,5 +129,5 @@ describe('App Page', () => {
 
     expect(addedSecondTask).toBeInTheDocument();
     expect(addedSecondTask).not.toHaveClass('completed');
-  })
-})
+  });
+});
